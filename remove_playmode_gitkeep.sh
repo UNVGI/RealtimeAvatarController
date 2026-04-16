9e5128d46a8e9585aaf38e87bfed11bc89e6e6a5
Hidano <n.hidano@hidano-dev.com>
2026-04-16 09:54:47 +0900

    Add remove_playmode_gitkeep.sh

---
+++ remove_playmode_gitkeep.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+PKG="RealtimeAvatarController/Packages/com.cysharp.realtimeavatarcontroller/Tests/PlayMode"
+rm -f "$PKG/slot-core/.gitkeep" "$PKG/motion-pipeline/.gitkeep" "$PKG/mocap-vmc/.gitkeep" "$PKG/avatar-provider-builtin/.gitkeep"
