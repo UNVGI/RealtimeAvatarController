eb9625dc7f42d8831a3f3a43d936a7921058b1ce
Hidano <n.hidano@hidano-dev.com>
2026-04-16 09:51:12 +0900

    Add remove_gitkeep.sh

---
+++ remove_gitkeep.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -f "RealtimeAvatarController/Packages/com.cysharp.realtimeavatarcontroller/Tests/EditMode/slot-core/.gitkeep"
+rm -f "RealtimeAvatarController/Packages/com.cysharp.realtimeavatarcontroller/Tests/EditMode/motion-pipeline/.gitkeep"
+rm -f "RealtimeAvatarController/Packages/com.cysharp.realtimeavatarcontroller/Tests/EditMode/mocap-vmc/.gitkeep"
+rm -f "RealtimeAvatarController/Packages/com.cysharp.realtimeavatarcontroller/Tests/EditMode/avatar-provider-builtin/.gitkeep"
