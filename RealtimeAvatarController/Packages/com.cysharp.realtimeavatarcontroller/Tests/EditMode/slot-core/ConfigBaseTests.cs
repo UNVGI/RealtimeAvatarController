c3ec1840fffd4aefd5e24397a611236054cb790f
Hidano <n.hidano@hidano-dev.com>
2026-04-16 10:48:12 +0900

    2.2 MoCapSourceConfigBase を TDD で実装する
    - Runtime/Core/Configs/MoCapSourceConfigBase.cs を新規作成 (public abstract class MoCapSourceConfigBase : ScriptableObject)
    - Tests/EditMode/slot-core/ConfigBaseTests.cs に MoCapSourceConfigBase 用テスト4件を追加 (SO継承確認, abstract確認, CreateInstance確認, 基底型キャスト確認)

--- RealtimeAvatarController/Packages/com.cysharp.realtimeavatarcontroller/Tests/EditMode/slot-core/ConfigBaseTests.cs
+++ RealtimeAvatarController/Packages/com.cysharp.realtimeavatarcontroller/Tests/EditMode/slot-core/ConfigBaseTests.cs
@@ -38,5 +38,56 @@ namespace RealtimeAvatarController.Core.Tests
                 Object.DestroyImmediate(instance);
             }
         }
+
+        // --- MoCapSourceConfigBase テスト ---
+
+        /// <summary>
+        /// テスト用の具象サブクラス。abstract クラスは直接 CreateInstance できないため。
+        /// </summary>
+        private class ConcreteMoCapSourceConfig : MoCapSourceConfigBase { }
+
+        [Test]
+        public void MoCapSourceConfigBase_InheritsFromScriptableObject()
+        {
+            Assert.That(typeof(MoCapSourceConfigBase).IsSubclassOf(typeof(ScriptableObject)), Is.True);
+        }
+
+        [Test]
+        public void MoCapSourceConfigBase_IsAbstract()
+        {
+            Assert.That(typeof(MoCapSourceConfigBase).IsAbstract, Is.True);
+        }
+
+        [Test]
+        public void MoCapSourceConfigBase_ConcreteSubclass_CanBeCreatedViaCreateInstance()
+        {
+            var instance = ScriptableObject.CreateInstance<ConcreteMoCapSourceConfig>();
+            try
+            {
+                Assert.That(instance, Is.Not.Null);
+                Assert.That(instance, Is.InstanceOf<MoCapSourceConfigBase>());
+                Assert.That(instance, Is.InstanceOf<ScriptableObject>());
+            }
+            finally
+            {
+                Object.DestroyImmediate(instance);
+            }
+        }
+
+        [Test]
+        public void MoCapSourceConfigBase_ConcreteSubclass_CanBeCastToBaseType()
+        {
+            var instance = ScriptableObject.CreateInstance<ConcreteMoCapSourceConfig>();
+            try
+            {
+                MoCapSourceConfigBase baseRef = instance;
+                Assert.That(baseRef, Is.Not.Null);
+                Assert.That(baseRef, Is.SameAs(instance));
+            }
+            finally
+            {
+                Object.DestroyImmediate(instance);
+            }
+        }
     }
 }
